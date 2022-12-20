6a9fbe9bdce30e0e2fdbd02caf3d2a0dfcf4ef62
Joseph Karnafel <joseph@riff.agency>
2022-12-20 11:37:52 -0800

    add dark theme and pit bull picture

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from "react";
 import Layout from "../components/layout.js";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pit bull, posing on a couch and looking stoically at the camera"
+        src="../images/pitbull.jpeg"
+      />
     </Layout>
   );
 };
